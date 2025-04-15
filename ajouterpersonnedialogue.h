a2a475fc0723462438bc72cc3b2373c20a6e01ec
Maria Bouakkaz <maria.bouakkaz@fs.ummto.dz>
2025-04-15 16:44:56 +0200

    ajout de  ajouterpersonnedialogue.h

---
+++ ajouterpersonnedialogue.h
@@ -0,0 +1,27 @@
+#ifndef AJOUTERPERSONNEDIALOGUE_H
+#define AJOUTERPERSONNEDIALOGUE_H
+#include "include/ldc_personne.h"
+#include <QTableWidget>
+#include <QPushButton>
+#include <QWidget>
+#include <QDialog>
+
+class AjouterPersonneDialogue : public QDialog
+{
+    Q_OBJECT
+    friend class dialoguePersonne;
+public:
+    AjouterPersonneDialogue(LDC_personne* personnes=nullptr, QWidget *parent = nullptr);
+private:
+    LDC_personne* d_personnes;
+
+    QLineEdit *d_EditNom;
+    QLineEdit *d_EditPrenom;
+    QLineEdit *d_EditTel;
+    QLineEdit *d_EditEmail;
+
+    void creeInterface();
+signals:
+};
+
+#endif // AJOUTERPERSONNEDIALOGUE_H
